a0cf57c05bc9d716b5643e0241f18c952c31bc9a
BrunoMatosSilva <brunomatos2010@outlook.com>
2022-05-06 12:02:45 -0300

    correction css navlink

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -20,11 +20,11 @@ export const Container = styled.header`
 
     > a {
     text-transform: uppercase;
-    color: ${(props) => props.isActive ? props.theme.primary : props.theme.textHighlight};
+    color: ${(props) => props.theme.textHighlight};
     transition: 0.5s;
 
     &:hover{
-        color: ${(props) => props.isActive ? lighten(0.2, props.theme.primary) : lighten(0.2, props.theme.textHighlight)};
+        color: ${(props) => lighten(0.2, props.theme.textHighlight)};
     }
 }
  }
