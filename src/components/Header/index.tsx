c973b2b1f45b47561e552f2a5dc76fbce9a774b8
BrunoMatosSilva <brunomatos2010@outlook.com>
2022-05-06 11:58:14 -0300

    form orçamento

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,6 +7,12 @@ function Header() {
       <ul>
         <NavLink title="Home" path="/" />
         <NavLink title="Projetos" path="/projetos" includes />
+        <a
+          href="https://my.forms.app/form/627523b3b838185b935d451a"
+          target="_blank"
+        >
+          Orçamento
+        </a>
       </ul>
     </Container>
   );
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -17,6 +17,16 @@ export const Container = styled.header`
     display: flex;
     gap: 2rem;
     align-items: center;
+
+    > a {
+    text-transform: uppercase;
+    color: ${(props) => props.isActive ? props.theme.primary : props.theme.textHighlight};
+    transition: 0.5s;
+
+    &:hover{
+        color: ${(props) => props.isActive ? lighten(0.2, props.theme.primary) : lighten(0.2, props.theme.textHighlight)};
+    }
+}
  }
 `;
 
